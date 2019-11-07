89e63becfbd5c9aa5c6664a3a2b02005dc364aa0
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2019-11-07 14:28:08 -0500

    Organization

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,11 @@
 if('serviceWorker' in navigator){
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/serviceworker.js')
-        .then((reg) => {
-          //console.log('Service worker registered.', reg);
-        }).catch(function(err){
-            //console.error("Service worker registration failed with error "+err);
-        });
+      .then((reg) => {
+        //console.log('Service worker registered.', reg);
+      }).catch(function(err){
+        //console.error("Service worker registration failed with error "+err);
+      });
   });
 } else {
   //console.error("Service worker registration failed.");
