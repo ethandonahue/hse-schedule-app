1f53a0c47ca6582e48ce647cd47ab76e2fb4c49f
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2019-11-08 13:28:17 -0500

    Time Monitorer

--- js/timeMonitor.js
+++ js/timeMonitor.js
@@ -1,3 +1,33 @@
+const schedulesPerWeek = {
+  "Sunday":false,
+  "Monday":"mondaySmartPeriodDay",
+  "Tuesday":"regularSchoolDay",
+  "Wednesday":"regularSchoolDay",
+  "Thursday":"regularSchoolDay",
+  "Friday":"regularSchoolDay",
+  "Saturday":false
+};
+
 readFileOnline("/json/schedules.json", (data) =>{
-  schedules = data;
+  schedules = JSON.parse(data);
+  currentSchedule = schedules[schedulesPerWeek[TimePlus.getCurrentDate().dayName]];
+  timeMonitor = setInterval(refresh, 100);
 });
+
+function refresh(){
+  var time = TimePlus.getCurrentTime();
+  if(time.hour >= currentSchedule.info.schoolStartTime.hour && time.hour <= currentSchedule.info.schoolEndTime.hour){
+    var period = getCurrentPeriod(Time);
+    updateTimeRemainingDiv();
+  } else {
+    updateTimeRemainingDiv("Not School Hours");
+  }
+}
+
+function updateTimeRemainingDiv(hour, minute, second){
+
+}
+
+function getCurrentPeriod(hour, minute){
+
+}
