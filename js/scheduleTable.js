69eb2a24e5729c33bdd10313a972ceb5f6b4aecc
Ethan <donaheth000@hsestudents.org>
2019-11-15 10:17:15 -0500

    Formatting

--- js/scheduleTable.js
+++ js/scheduleTable.js
@@ -1,80 +1,76 @@
 var schedule;
 
-function loadSchedule(){
-  if(currentSchedule.schedule != undefined){
-    schedule = currentSchedule.schedule;
-    clearInterval(interval);
-    createTable();
-  }
+function loadSchedule() {
+	if (currentSchedule.schedule != undefined) {
+		schedule = currentSchedule.schedule;
+		clearInterval(interval);
+		createTable();
+	}
 }
 
 var interval = setInterval(loadSchedule, 100);
 
 
-function createTable(){
+function createTable() {
 
-  for(var i = 0; i<schedule.length; i++){
-  if(schedule[i].passing){
-    i++
-  }
+	for (var i = 0; i < schedule.length; i++) {
+		if (schedule[i].passing) {
+			i++
+		}
 
-  var tableContainer = document.createElement("tr");
-  var tableRow1 = document.createElement("td");
-  var tableRow2 = document.createElement("td");
-  var content1 = document.createTextNode(schedule[i].periodNum);
-  var content2 = document.createTextNode(convertTime(schedule[i].startTime, schedule[i].endTime));
+		var tableContainer = document.createElement("tr");
+		var tableRow1 = document.createElement("td");
+		var tableRow2 = document.createElement("td");
+		var content1 = document.createTextNode(schedule[i].periodNum);
+		var content2 = document.createTextNode(convertTime(schedule[i].startTime, schedule[i].endTime));
 
-  tableRow1.appendChild(content1);
-  tableRow2.appendChild(content2);
+		tableRow1.appendChild(content1);
+		tableRow2.appendChild(content2);
 
-  tableRow1.setAttribute("class", "row");
-  tableRow2.setAttribute("class", "row");
+		tableRow1.setAttribute("class", "row");
+		tableRow2.setAttribute("class", "row");
 
-  tableContainer.appendChild(tableRow1);
-  tableContainer.appendChild(tableRow2);
+		tableContainer.appendChild(tableRow1);
+		tableContainer.appendChild(tableRow2);
 
-  var element = document.getElementsByTagName("table")[0];
-  element.appendChild(tableContainer);
+		var element = document.getElementsByTagName("table")[0];
+		element.appendChild(tableContainer);
 
-  }
+	}
 }
 
-function convertTime(start, end){
-  var output = "";
-  var startHour;
-  var endHour;
-  var startMinute;
-  var endMinute;
-  if(start.hour <= 12){
-    startHour = start.hour;
-  } else {
-    startHour = start.hour-12;
-  }
+function convertTime(start, end) {
+	var output = "";
+	var startHour;
+	var endHour;
+	var startMinute;
+	var endMinute;
+	if (start.hour <= 12) {
+		startHour = start.hour;
+	} else {
+		startHour = start.hour - 12;
+	}
 
-  if(end.hour <= 12){
-    endHour = end.hour;
-  } else {
-    endHour = end.hour-12;
-  }
+	if (end.hour <= 12) {
+		endHour = end.hour;
+	} else {
+		endHour = end.hour - 12;
+	}
 
-  if(start.minute < 10){
-    startMinute = "0" + start.minute;
-  } else {
-    startMinute = start.minute;
-  }
+	if (start.minute < 10) {
+		startMinute = "0" + start.minute;
+	} else {
+		startMinute = start.minute;
+	}
 
-  if(end.Minute < 10){
-    endMinute = "0" + end.minute;
-  } else {
-    endMinute = end.minute;
-  }
+	if (end.Minute < 10) {
+		endMinute = "0" + end.minute;
+	} else {
+		endMinute = end.minute;
+	}
 
+	output += startHour + ":" + startMinute + " - " + endHour + ":" + endMinute;
 
-  output += startHour + ":" + startMinute + " - " + endHour + ":" + endMinute;
-
-
-
-return output;
-
+	return output;
 
 }
