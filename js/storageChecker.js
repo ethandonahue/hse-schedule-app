9a4fb54fe07d33354c958123ec0bb42c446ec116
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2019-11-26 14:22:52 -0500

    Make Sure Lunch Is Entered

--- js/storageChecker.js
+++ js/storageChecker.js
@@ -1,16 +1,15 @@
-if(typeof(Storage) != "undefined"){
+if(typeof(Storage) != undefined){
   if(localStorage.selectedLunch != undefined){
     if(window.location.pathname == "/" || window.location.pathname == "/index.html"){
       window.location.replace("/screens/online/home.html");
     }
   } else {
-    if(window.location.pathname == "/" && window.location.pathname != "/index.html"){
+    if(window.location.pathname != "/" && window.location.pathname != "/index.html"){
       window.location.replace("/index.html");
     }
   }
-  //console.log("LocalStorage Was Able To Be Accessed");
 } else {
-  //console.log("LocalStorage Is Unable To Be Accessed");
+
 }
 
 function setLunch(lunch){
