bfc970ab36ac946d198f434226e38a2794bcd1fe
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-01-14 13:58:37 -0500

    Calendar Styling

--- js/calendarScript.js
+++ js/calendarScript.js
@@ -2,26 +2,31 @@ var calendarDayToPrint = 1;
 
 document.getElementById("cal-month").innerHTML = TimePlus.getCurrentDate().monthName + "<br>" + TimePlus.getCurrentDate().year;
 
-for(var w = 0; w < 5; w++){
-	for(var d = 0; d < 7; d++){
-		var wk = document.getElementById("cal-week-" + (w + 1));
-		var dy = document.createElement("td");
-		dy.setAttribute("class", "cal-days");
-		if(w == 0 && d < TimePlus.getCurrentDate().firstDayOfMonth || calendarDayToPrint == TimePlus.getCurrentDate().daysInMonth){
-			dy.innerHTML = "";
-		} else {
-			dy.innerHTML = calendarDayToPrint;
-			if(schedules[schedulesPerWeek[calendarDayToPrint - 1]].schedule){
-				dy.setAttribute("onclick", "displaySelectedSchedule(" + calendarDayToPrint + ")");
+SheetsPlus.whenNotEquals("currentSchedule", "undefined", generateCalendar);
+
+function generateCalendar(){
+	for(var w = 0; w < 5; w++){
+		for(var d = 0; d < 7; d++){
+			var wk = document.getElementById("cal-week-" + (w + 1));
+			var dy = document.createElement("td");
+			dy.setAttribute("class", "cal-days");
+			if(w == 0 && d < TimePlus.getCurrentDate().firstDayOfMonth || calendarDayToPrint == TimePlus.getCurrentDate().daysInMonth){
+				dy.innerHTML = "";
+				dy.setAttribute("class", "calendarOtherMonth");
 			} else {
-				dy.setAttribute("class", "calendarGrayBackground");
+				dy.innerHTML = calendarDayToPrint;
+				if(schedules[schedulesPerWeek[calendarDayToPrint - 1]].schedule){
+					dy.setAttribute("onclick", "displaySelectedSchedule(" + calendarDayToPrint + ")");
+				} else {
+					dy.setAttribute("class", "calendarNoSchool");
+				}
+				if(calendarDayToPrint == TimePlus.getCurrentDate().dayOfMonth){
+					dy.setAttribute("class", "calendarCurrentDay");
+				}
+				calendarDayToPrint++;
 			}
-			if(calendarDayToPrint == TimePlus.getCurrentDate().dayOfMonth){
-				dy.setAttribute("class", "calendarCurrentDay");
-			}
-			calendarDayToPrint++;
+			wk.appendChild(dy);
 		}
-		wk.appendChild(dy);
 	}
 }
 
