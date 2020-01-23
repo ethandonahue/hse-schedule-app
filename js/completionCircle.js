2d8fb06493efa626301a32c4c195c343a799e21c
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-01-23 10:51:25 -0500

    Better Circle

--- js/completionCircle.js
+++ js/completionCircle.js
@@ -23,32 +23,39 @@ function getCanvas(){
 
 function resizeCanvas(){
   if(landOrPort() == "landscape"){
-    canvas.width = window.innerWidth * 0.5;
-    canvas.height = window.innerHeight * 0.8;
+    canvas.style.width = window.innerWidth * 0.5 + "px";
+    canvas.style.height = window.innerHeight * 0.8 + "px";
+    canvas.width = window.innerWidth * 0.5 * window.devicePixelRatio;
+    canvas.height = window.innerHeight * 0.8 * window.devicePixelRatio;
   } else {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight * 0.35;
+    canvas.style.width = window.innerWidth + "px";
+    canvas.style.height = window.innerHeight * 0.35 + "px";
+    canvas.width = window.innerWidth * window.devicePixelRatio;
+    canvas.height = window.innerHeight * 0.35 * window.devicePixelRatio;
   }
+  surface.scale(window.devicePixelRatio, window.devicePixelRatio);
   completeCircle(1 - TimePlus.toSeconds(TimePlus.timeBetween(TimePlus.getCurrentTime(), period.endTime)) / TimePlus.toSeconds(TimePlus.timeBetween(period.startTime, period.endTime)));
 }
 
 function completeCircle(percentage){
-  var radius = canvas.width / 3;
+  var width = canvas.style.width.substring(0, canvas.style.width.indexOf("px"));
+  var height = canvas.style.height.substring(0, canvas.style.height.indexOf("px"));
+  var radius = width / 3;
   var endingPos = (percentage * 2 * Math.PI) - (0.5 * Math.PI);
   if(endingPos == startingPos){
     endingPos = 3.5 * Math.PI;
   }
   if(landOrPort() == "portrait"){
-    radius = canvas.height / 2.1;
+    radius = height / 2.1;
   }
   surface.beginPath();
   surface.strokeStyle = bgColorCircle;
   surface.lineWidth = 2;
-  surface.arc(canvas.width / 2, canvas.height / 2, radius, 0, 2 * Math.PI);
+  surface.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
   surface.stroke();
   surface.beginPath();
   surface.strokeStyle = fgColorCircle;
   surface.lineWidth = 5;
-  surface.arc(canvas.width / 2, canvas.height / 2, radius, startingPos, endingPos);
+  surface.arc(width / 2, height / 2, radius, startingPos, endingPos);
   surface.stroke();
 }
