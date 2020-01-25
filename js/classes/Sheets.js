c75b968c194f0653298a98726e793d17edcee9a5
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-01-25 00:58:43 -0500

    Creation Of Sheets.js

---
+++ js/classes/Sheets.js
@@ -0,0 +1,47 @@
+//Created By: Isaac Robbins
+//For Use With: HSE Schedule App
+
+loadGoogleCharts();
+
+function Sheet(url){
+  this.url = url;
+  this.rawData = false;
+
+  var me = this;
+
+  this.getRawData = function(){
+    var me = this;
+    this._makeQuery();
+    return new Promise((resolve) => {
+      this._whenRawData(() => {
+        resolve(this.rawData);
+      });
+    });
+  }
+
+  this._makeQuery = function(){
+    var query = new google.visualization.Query(this.url);
+    query.send(this._handleQueryResponse);
+  }
+
+  this._handleQueryResponse = function(response){
+    me.rawData = response.getDataTable();
+  }
+
+  this._whenRawData = function(callback){
+    var interval = setInterval(() => {
+      try{
+        if(this.rawData != false){
+          clearInterval(interval);
+          callback();
+        }
+      } catch {
+
+      }
+    }, 100, interval, callback);
+  }
+}
+
+function loadGoogleCharts(){
+  google.charts.load('current', {'packages':['corechart']});
+}
