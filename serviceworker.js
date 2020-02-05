8cf7550b6863b94442964130fae85098554655c5
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-02-05 14:30:55 -0500

    Fixed serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,71 +1,85 @@
-const CACHE_NAME = 'HSEScheduleAppCache';
+const CACHE_NAME = "HSEScheduleAppCache";
 
 const FILES_TO_CACHE = [
-  '/index.html',
-  '/main.html',
-  '/styling/index.css',
-  '/styling/notifications.css',
-  '/styling/bellSchedule.css',
-  '/styling/home.css',
-  '/styling/calendar.css',
-  '/styling/settings.css',
-  '/styling/style.css',
-  '/manifest.json',
-  '/serviceworker.js',
-  '/js/storageChecker.js',
-  '/js/ScriptPlus.js',
-  '/js/timeMonitor.js',
-  '/js/scheduleTable.js',
-  '/js/themeSwitcher.js',
-  '/images/HSEScheduleAppLogo192.png',
-  '/images/HSEScheduleAppLogo512.png',
-  '/images/selected/notifications.png',
-  '/images/selected/schedule.png',
-  '/images/selected/home.png',
-  '/images/selected/calendar.png',
-  '/images/selected/settings.png',
-  '/images/unselected/notifications.png',
-  '/images/unselected/schedule.png',
-  '/images/unselected/home.png',
-  '/images/unselected/calendar.png',
-  '/images/unselected/settings.png'
+  "/",
+  "/index.html",
+  "/main.html",
+  "/manifest.json",
+  "/serviceworker.js",
+
+  "/js/classes/DateTime.js",
+  "/js/classes/Sheets.js",
+  "/js/classes/Schedule.js",
+  "/js/classes/PopUp.js",
+  "/js/classes/Storage.js",
+
+  "/js/main.js",
+  "/js/ScriptPlus.js",
+  "/js/pageSwitcher.js",
+  "/js/scheduleTable.js",
+  "/js/calendarScript.js",
+  "/js/storageChecker.js",
+  "/js/themeSwitcher.js",
+  "/js/completionCircle.js",
+  "/js/twitterFeed.js",
+
+  "/styling/index.css",
+  "/styling/style.css",
+  "/styling/about.css",
+  "/styling/bellSchedule.css",
+  "/styling/home.css",
+  "/styling/calendar.css",
+  "/styling/settings.css",
+
+
+  "/images/HSEScheduleAppLogo192.png",
+  "/images/HSEScheduleAppLogo512.png",
+
+  "/images/isaac.jpg",
+  "/images/ethan.jpg",
+
+  "/images/selected/about.png",
+  "/images/selected/schedule.png",
+  "/images/selected/home.png",
+  "/images/selected/calendar.png",
+  "/images/selected/settings.png",
+
+  "/images/unselected/about.png",
+  "/images/unselected/schedule.png",
+  "/images/unselected/home.png",
+  "/images/unselected/calendar.png",
+  "/images/unselected/settings.png"
 ];
 
 const publicKey = "BHV9vDKgZXPZH3S--ZPlDH4R4LQ636jvztTtYQppjrpVfJY3btRPzFhuvGY_xFrvpvCeAvMnJ7p3Vh2rykeaV54";
 
-if('serviceWorker' in navigator){ //&& 'PushManager' in window){
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceworker.js')
+if("serviceWorker" in navigator){
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/serviceworker.js")
       .then((reg) => {
         registration = reg;
-        //checkNotificationSubscription();
-        //console.log('Service worker registered.', reg);
       }).catch(function(err){
-        //console.error("Service worker registration failed with error "+err);
+
       });
   });
 } else {
-  //console.error("Service worker registration failed.");
+
 }
 
-self.addEventListener('install', (evt) => {
-  //console.log('[ServiceWorker] Install');
+self.addEventListener("install", (evt) => {
   evt.waitUntil(
       caches.open(CACHE_NAME).then((cache) => {
-        //console.log('[ServiceWorker] Pre-caching offline page');
         return cache.addAll(FILES_TO_CACHE);
       })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  //console.log('[ServiceWorker] Activate');
+self.addEventListener("activate", (evt) => {
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          //console.log('[ServiceWorker] Removing old cache', key);
+        if (key != CACHE_NAME) {
           return caches.delete(key);
         }
       }));
@@ -74,9 +88,8 @@ self.addEventListener('activate', (evt) => {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (evt) => {
-  //console.log('[ServiceWorker] Fetch', evt.request.url);
-  if (evt.request.mode !== 'navigate') {
+self.addEventListener("fetch", (evt) => {
+  if (evt.request.mode != "navigate") {
     return;
   }
   evt.respondWith(
@@ -84,7 +97,7 @@ self.addEventListener('fetch', (evt) => {
           .catch(() => {
             return caches.open(CACHE_NAME)
                 .then((cache) => {
-                  return cache.match('/main.html');
+                  return cache.match("/main.html");
                 });
           })
   );
