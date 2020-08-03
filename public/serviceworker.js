3a7e4295436983e20bdde3dda08fdba984aa5edb
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-08-03 16:47:37 -0400

    updated service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,54 +1,7 @@
 const CACHE_NAME = "HSEScheduleAppCache";
 
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/main.html",
-  "/manifest.json",
-  "/serviceworker.js",
-
-  "/js/classes/DateTime.js",
-  "/js/classes/Sheets.js",
-  "/js/classes/Schedule.js",
-  "/js/classes/PopUp.js",
-  "/js/classes/Storage.js",
-
-  "/js/main.js",
-  "/js/ScriptPlus.js",
-  "/js/pageSwitcher.js",
-  "/js/scheduleTable.js",
-  "/js/calendarScript.js",
-  "/js/storageChecker.js",
-  "/js/themeSwitcher.js",
-  "/js/completionCircle.js",
-  "/js/twitterFeed.js",
-
-  "/styling/index.css",
-  "/styling/style.css",
-  "/styling/about.css",
-  "/styling/bellSchedule.css",
-  "/styling/home.css",
-  "/styling/calendar.css",
-  "/styling/settings.css",
-
-
-  "/images/HSEScheduleAppLogo192.png",
-  "/images/HSEScheduleAppLogo512.png",
-
-  "/images/isaac.jpg",
-  "/images/ethan.jpg",
-
-  "/images/selected/about.png",
-  "/images/selected/schedule.png",
-  "/images/selected/home.png",
-  "/images/selected/calendar.png",
-  "/images/selected/settings.png",
-
-  "/images/unselected/about.png",
-  "/images/unselected/schedule.png",
-  "/images/unselected/home.png",
-  "/images/unselected/calendar.png",
-  "/images/unselected/settings.png"
+  "/public"
 ];
 
 const publicKey = "BHV9vDKgZXPZH3S--ZPlDH4R4LQ636jvztTtYQppjrpVfJY3btRPzFhuvGY_xFrvpvCeAvMnJ7p3Vh2rykeaV54";
