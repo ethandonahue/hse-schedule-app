7967ded7ebe8a267ba469b6d018495c91f694748
Ethan Donahue <donaheth000@hsestudents.org>
2020-09-15 09:48:55 -0400

    calendar fix

--- public/js/calendarScript.js
+++ public/js/calendarScript.js
@@ -2,7 +2,7 @@ function generateCalendar(ms) {
   var calendarDayToPrint = 1;
   deleteCalendar();
   document.getElementById("cal-month").innerHTML = moment().format("MMMM") + "<br>" + moment().format("YYYY");
-  for (var w = 0; w < 6; w++) {
+  for (var w = 0; w < 5; w++) {
     for (var d = 0; d < 7; d++) {
       var wk = document.getElementById("cal-week-" + (w + 1));
       var dy = document.createElement("td");
@@ -10,15 +10,32 @@ function generateCalendar(ms) {
       if ((w == 0 && d < moment().startOf("month").day()) || calendarDayToPrint > ms.length) {
         dy.innerHTML = "";
         dy.setAttribute("class", "calendarOtherMonth");
+
       } else {
         dy.innerHTML = calendarDayToPrint;
         dy.setAttribute("onclick", "displaySelectedSchedule(" + (calendarDayToPrint - 1) + ")");
         if(ms[calendarDayToPrint - 1].metadata.type != "school day") {
           dy.setAttribute("class", "calendarNoSchool");
         }
-        if(calendarDayToPrint == moment().date()) {
-          dy.setAttribute("class", "calendarCurrentDay");
+        if(ms[calendarDayToPrint-1].metadata.dayColor == "red"){
+            dy.style.color = "red";
+        } else if (ms[calendarDayToPrint-1].metadata.dayColor == "blue"){
+            dy.style.color = "blue";
         }
+        // moment().date()
+        if(calendarDayToPrint == moment().date()) {
+          if(ms[calendarDayToPrint-1].metadata.dayColor == "red"){
+            dy.setAttribute("class", "calendarRedDay");
+          } else if (ms[calendarDayToPrint-1].metadata.dayColor == "blue"){
+            dy.setAttribute("class", "calendarBlueDay");
+          } else {
+            dy.setAttribute("class", "calendarCurrentDay");
+          }
+
+        }
+        console.log(ms[calendarDayToPrint-1].metadata.dayColor);
+
+
         calendarDayToPrint++;
       }
       wk.appendChild(dy);
